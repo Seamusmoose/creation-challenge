644aac0c8dc7a350deb844bbbce9558b7d8c6947
Seamus OSullivan <info@seamuso.com>
2023-03-03 13:53:08 +0100

    nav fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import Logo from "../../public/creaition_logo.svg";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className="flex space-bt">
       <Image src={Logo} height={100} width={100} alt="logo" />
       <ul className="nav">
-        <li>HomePage</li>
+        <li>
+          <Link href={"https://www.creaition.io/"}>HomePage</Link>
+        </li>
       </ul>
     </div>
   );
